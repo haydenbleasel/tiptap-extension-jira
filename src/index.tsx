4babbbd15dfc5099c10cb94ad1a87c6b02cec981
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-10 18:20:29 -0400

    Fix index files

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,29 @@
-import {} from '@tiptap/core';
-import {} from '@tiptap/react';
+import { DateNode } from './extensions/date';
+import { Em } from './extensions/em';
+import { ExpandClient } from './extensions/expand/client';
+import { InlineCard } from './extensions/inline-card';
+import { Media } from './extensions/media';
+import { MediaSingleClient } from './extensions/media-single/client';
+import { Panel } from './extensions/panel';
+import { Rule } from './extensions/rule';
+import { Status } from './extensions/status';
+import { Strong } from './extensions/strong';
+import { SubSup } from './extensions/subsup';
+import { TextColor } from './extensions/text-color';
+import { UndefinedNode } from './extensions/undefined';
 
-export const jiraClientExtensions = [MediaSingle, Expand];
+export const Jira = [
+  MediaSingleClient,
+  ExpandClient,
+  DateNode,
+  Em,
+  InlineCard,
+  Media,
+  Panel,
+  Rule,
+  Status,
+  Strong,
+  SubSup,
+  TextColor,
+  UndefinedNode,
+];
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,29 @@
-import {} from '@tiptap/core';
+import { DateNode } from './extensions/date';
+import { Em } from './extensions/em';
+import { ExpandServer } from './extensions/expand/server';
+import { InlineCard } from './extensions/inline-card';
+import { Media } from './extensions/media';
+import { MediaSingleServer } from './extensions/media-single/server';
+import { Panel } from './extensions/panel';
+import { Rule } from './extensions/rule';
+import { Status } from './extensions/status';
+import { Strong } from './extensions/strong';
+import { SubSup } from './extensions/subsup';
+import { TextColor } from './extensions/text-color';
+import { UndefinedNode } from './extensions/undefined';
 
-export const jiraServerExtensions = [MediaSingle, Expand];
+export const Jira = [
+  MediaSingleServer,
+  ExpandServer,
+  DateNode,
+  Em,
+  InlineCard,
+  Media,
+  Panel,
+  Rule,
+  Status,
+  Strong,
+  SubSup,
+  TextColor,
+  UndefinedNode,
+];
